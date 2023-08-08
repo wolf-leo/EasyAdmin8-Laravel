bb2b881a717bc7014192151f58b9940c2ab9d560
wolfcode <37436228+wolf-leo@users.noreply.github.com>
2023-08-08 11:24:42 +0800

    Update MallGoods.php

--- app/Models/MallGoods.php
+++ app/Models/MallGoods.php
@@ -2,6 +2,14 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Relations\HasOne;
+
 class MallGoods extends BaseModel
 {
+
+    public function cate(): HasOne
+    {
+        return $this->hasOne(MallCate::class, 'id', 'cate_id')->select('id', 'title');
+    }
+    
 }
