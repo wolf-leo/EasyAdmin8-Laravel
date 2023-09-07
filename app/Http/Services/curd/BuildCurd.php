e8a2cf4bea625c82e8ec0ad7cf98805a08d62ddd
wolfcode <37436228+wolf-leo@users.noreply.github.com>
2023-09-07 13:29:03 +0800

    Update BuildCurd.php

--- app/Http/Services/curd/BuildCurd.php
+++ app/Http/Services/curd/BuildCurd.php
@@ -364,7 +364,7 @@ class BuildCurd
                 'tableColumns'    => $formatColums,
             ];
             if (!empty($bindSelectField)) {
-                $relationArray                                      = explode('\\', $modelFilename);
+                $relationArray                                      = explode(DIRECTORY_SEPARATOR, $modelFilename);
                 $this->tableColumns[$foreignKey]['bindSelectField'] = $bindSelectField;
                 $this->tableColumns[$foreignKey]['bindRelation']    = end($relationArray);
             }
@@ -720,7 +720,7 @@ class BuildCurd
      */
     protected function buildRelationSelectModel($relation, $filed): mixed
     {
-        $relationArray = explode('\\', $relation);
+        $relationArray = explode(DIRECTORY_SEPARATOR, $relation);
         $name          = end($relationArray);
         $name          = "get{$name}List";
         $selectCode    = CommonTool::replaceTemplate(
@@ -988,7 +988,7 @@ class BuildCurd
         $selectList = '';
         foreach ($this->relationArray as $relation) {
             if (!empty($relation['bindSelectField'])) {
-                $relationArray = explode('\\', $relation['modelFilename']);
+                $relationArray = explode(DIRECTORY_SEPARATOR, $relation['modelFilename']);
                 $selectList    .= $this->buildSelectController(end($relationArray));
             }
         }
