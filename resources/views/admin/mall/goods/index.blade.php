8915f52825b09a0dcb8c69823a30b23bc5d5c7f2
wolfcode <37436228+wolf-leo@users.noreply.github.com>
2023-09-14 17:14:57 +0800

    Update index.blade.php

--- resources/views/admin/mall/goods/index.blade.php
+++ resources/views/admin/mall/goods/index.blade.php
@@ -2,7 +2,7 @@
 <div class="layuimini-container">
     <div class="layuimini-main">
         <table id="currentTable" class="layui-table layui-hide"
-               data-auth-add="{{auth('mall.goods/add')}}"
+               data-auth-add="{{auths('mall.goods/add')}}"
                data-auth-edit="{{auths('mall.goods/edit')}}"
                data-auth-delete="{{auths('mall.goods/delete')}}"
                data-auth-stock="{{auths('mall.goods/stock')}}"
