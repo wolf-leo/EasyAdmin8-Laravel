98264415f83524ee4590bdf4b615590c3f756a5b
wolfcode <37436228+wolf-leo@users.noreply.github.com>
2024-03-14 18:09:06 +0800

    Update easyadmin.php

--- config/easyadmin.php
+++ config/easyadmin.php
@@ -1,12 +1,12 @@
 <?php
 
 return [
-    'ADMIN'             => env('easyadmin.ADMIN', 'admin'),
-    'CAPTCHA'           => env('easyadmin.CAPTCHA', false),
-    'IS_DEMO'           => env('easyadmin.IS_DEMO', false),
-    'CDN'               => env('easyadmin.CDN', ''),
-    'EXAMPLE'           => env('easyadmin.EXAMPLE', true),
-    'IS_CSRF'           => env('easyadmin.IS_CSRF', false),
-    'STATIC_PATH'       => env('easyadmin.STATIC_PATH', '/static'),
-    'OSS_STATIC_PREFIX' => env('easyadmin.OSS_STATIC_PREFIX', 'static_easyadmin'),
+    'ADMIN'             => env('EASYADMIN.ADMIN', 'admin'),
+    'CAPTCHA'           => env('EASYADMIN.CAPTCHA', false),
+    'IS_DEMO'           => env('EASYADMIN.IS_DEMO', false),
+    'CDN'               => env('EASYADMIN.CDN', ''),
+    'EXAMPLE'           => env('EASYADMIN.EXAMPLE', true),
+    'IS_CSRF'           => env('EASYADMIN.IS_CSRF', false),
+    'STATIC_PATH'       => env('EASYADMIN.STATIC_PATH', '/static'),
+    'OSS_STATIC_PREFIX' => env('EASYADMIN.OSS_STATIC_PREFIX', 'static_easyadmin'),
 ];
