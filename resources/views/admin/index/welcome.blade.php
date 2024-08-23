4e9f1b8bedfaae1778e4bad678a366baf62fff84
wolfcode <37436228+wolf-leo@users.noreply.github.com>
2024-08-23 16:38:21 +0800

    fix(styles): remove duplicate welcome.css link tag
    A duplicate link tag for welcome.css was removed to prevent potential styling issues and
    ensure the stylesheet is loaded only once, improving page load efficiency.

--- resources/views/admin/index/welcome.blade.php
+++ resources/views/admin/index/welcome.blade.php
@@ -1,6 +1,5 @@
 @include('admin.layout.head')
 <link rel="stylesheet" href="/static/admin/css/welcome.css?v={{$version}}" media="all">
-<link rel="stylesheet" href="/static/admin/css/welcome.css?v={$version}" media="all">
 <div class="layui-layout layui-padding-2">
     <div class="layui-layout-admin">
         <div class="layui-row layui-col-space10">
