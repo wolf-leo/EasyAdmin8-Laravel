a9e16cc87c372a6d481cb929a9a3ecb3d2377c66
wolfcode <37436228+wolf-leo@users.noreply.github.com>
2024-10-09 15:23:27 +0800

    Create logviewer.php

---
+++ config/logviewer.php
@@ -0,0 +1,12 @@
+<?php
+return [
+
+    // 日志标题
+    'title'          => 'EasyAdmin8框架日志',
+
+    // layui css 路径
+    'layui_css_path' => '/static/plugs/layui-v2.x/css/layui.css',
+    // layui js 路径
+    'layui_js_path'  => '/static/plugs/layui-v2.x/layui.js',
+
+];
