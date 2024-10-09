8e470a8c1a7667d07151eb3a61c8874b76cd9e81
wolfcode <37436228+wolf-leo@users.noreply.github.com>
2024-10-09 15:23:34 +0800

    Update index.blade.php

--- resources/views/admin/system/log/index.blade.php
+++ resources/views/admin/system/log/index.blade.php
@@ -1,7 +1,9 @@
 @include('admin.layout.head')
 <div class="layuimini-container">
     <div class="layuimini-main">
-        <table id="currentTable" class="layui-table layui-hide" lay-filter="currentTable">
+        <table id="currentTable" class="layui-table layui-hide"
+               data-auth-record="{:auth('system.log/record')}"
+               lay-filter="currentTable">
         </table>
     </div>
 </div>
