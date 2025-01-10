6ad1b0a4ff8b9551324d1e7851b6f6e33fe658a5
wolfcode <37436228+wolf-leo@users.noreply.github.com>
2025-01-10 10:31:11 +0800

    fix(MenuService): add URL validation for menu items
    - Implement URL validation for menu items to ensure proper URL formatting-Add a conditional check to prepend admin alias only if the URL is not valid
    - Simplify the map function callback by removing unnecessary type casting

--- app/Http/Services/MenuService.php
+++ app/Http/Services/MenuService.php
@@ -43,7 +43,7 @@ class MenuService
         $treeList = [];
         foreach ($menuList as $v) {
             $check = empty($v['href']) || $authServer->checkNode($v['href']);
-            !empty($v['href']) && $v['href'] = '/' . $this->adminConfig['admin_alias_name'] . "/{$v['href']}";
+            !empty($v['href']) && $v['href'] = filter_var($v['href'], FILTER_VALIDATE_URL) ? $v['href'] : '/' . $this->adminConfig['admin_alias_name'] . "/{$v['href']}";
             if ($pid == $v['pid'] && $check) {
                 $node  = $v;
                 $child = $this->buildMenuChild($v['id'], $menuList, $authServer);
@@ -65,7 +65,7 @@ class MenuService
             ->whereNull('delete_time')
             ->where([['status', '=', '1'], ['pid', '<>', HOME_PID]])
             ->orderByDesc('sort')->orderBy('id')
-            ->get()->map(function ($value) {
+            ->get()->map(function($value) {
                 return (array)$value;
             })->toArray();
         return $menuData;
